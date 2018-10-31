8e36ac755a951d65e0a6655f4f41502114d08d40
Surinam <extensionsapp@gmail.com>
2018-10-31 23:48:08 +0200

    Change size

--- progre.sh
+++ progre.sh
@@ -10,7 +10,7 @@ progreSh() {
     if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
     SEC=`printf "%04d\n" $(($(date +"%s")-${TME}))`; SEC="$SEC sec"
     PRC=`printf "%.0f" ${1}`
-    SHW=`printf "% 3d\n" ${PRC}`
+    SHW=`printf "%3d\n" ${PRC}`
     LNE=`printf "%.0f" $((${PRC}/2))`
     LRR=`printf "%.0f" $((${PRC}/2-12))`; if [ ${LRR} -le 0 ]; then LRR=0; fi;
     LYY=`printf "%.0f" $((${PRC}/2-24))`; if [ ${LYY} -le 0 ]; then LYY=0; fi;
@@ -24,28 +24,28 @@ progreSh() {
     do
     	DOTS=""; for ((ii=${i};ii<13;ii++)); do DOTS="${DOTS}."; done
     	if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............  ${SHW}%${NC}\r"
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
     	if [ ${LNE} -ge 1 ]; then sleep .05; fi
     done
     for ((i=14;i<=25;i++))
     do
     	DOTS=""; for ((ii=${i};ii<25;ii++)); do DOTS="${DOTS}."; done
     	if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............  ${SHW}%${NC}\r"
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
     	if [ ${LNE} -ge 14 ]; then sleep .05; fi
     done
     for ((i=26;i<=37;i++))
     do
     	DOTS=""; for ((ii=${i};ii<37;ii++)); do DOTS="${DOTS}."; done
     	if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............  ${SHW}%${NC}\r"
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
     	if [ ${LNE} -ge 26 ]; then sleep .05; fi
     done
     for ((i=38;i<=49;i++))
     do
     	DOTS=""; for ((ii=${i};ii<49;ii++)); do DOTS="${DOTS}."; done
     	if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS}  ${SHW}%${NC}\r"
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
     	if [ ${LNE} -ge 38 ]; then sleep .05; fi
     done
 }
